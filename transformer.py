ac0c4059513ae89575eaadba818bb763ad38b99d
thuongtuandang <dangtuanthuong@gmail.com>
2023-11-26 18:02:13 +0100

    complete version with multi heads

--- transformer.py
+++ transformer.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.optim as opt
 from dataset import Dataset
-from utils import self_attention
+from utils import initialize_matrix, stack_heads, self_attention, manually_update
 import torch.nn.init as init
 import math
 
@@ -12,38 +12,16 @@ class Transformer():
     # dim_input_size = dim of word embedding, in this case it is vocab_size
     def __init__(self, input_size, dim_input_size, output_size, hidden_size = 64):
         # That is the number of heads
-        self.n_heads = 1
+        self.n_heads = 2
         # Here we can define matrices in pytorch with require_grad = True
         # Initialize matrices for self-attention layer
-        self.Wq = torch.empty(dim_input_size, 0)
-        self.Wk = torch.empty(dim_input_size, 0)
-        self.Wv = torch.empty(dim_input_size, 0)
-        for i in range(self.n_heads):
-            # Initialize matrices with Xavier initialization
-            Wq = torch.empty(dim_input_size, hidden_size)
-            init.xavier_normal_(Wq)
-            Wq = Wq/1000
-            self.Wq = torch.cat((self.Wq, Wq), dim = 1)
-
-            Wk = torch.empty(dim_input_size, hidden_size)
-            init.xavier_normal_(Wk)
-            Wk = Wk/1000
-            self.Wk = torch.cat((self.Wk, Wk), dim = 1)
-
-            Wv = torch.empty(dim_input_size, hidden_size)
-            init.xavier_normal_(Wv)
-            Wv = Wv/1000
-            self.Wv = torch.cat((self.Wv, Wv), dim = 1)
-        
-        self.Wq.requires_grad = True
-        self.Wk.requires_grad = True
-        self.Wv.requires_grad = True
+        self.Wq = stack_heads(self.n_heads, dim_input_size, hidden_size) 
+        self.Wk = stack_heads(self.n_heads, dim_input_size, hidden_size)
+        self.Wv = stack_heads(self.n_heads, dim_input_size, hidden_size)
 
         # Initialize the matrix Wo for the projection of the multi head layer output
-        self.Wo = torch.empty(self.n_heads * hidden_size, hidden_size)
-        init.xavier_normal_(self.Wo)
-        Wo = torch.sigmoid(self.Wo)
-        self.Wo.requires_grad = True
+        self.Wo = initialize_matrix(self.n_heads * hidden_size, hidden_size)
+
         # Create the mask matrix with upper part is -infinity
         # and other entries are 0
         # First we create a matrix filled with - infinity at all entries
@@ -57,64 +35,61 @@ class Transformer():
         self.n = 10000
 
         # Gamma and beta is for the normalization layer
-        self.gamma_self = torch.empty((dim_input_size + hidden_size, hidden_size))
-        init.xavier_normal_(self.gamma_self)
-        self.gamma_self.requires_grad = True
+        self.gamma_self = initialize_matrix(dim_input_size + hidden_size, hidden_size)
         self.beta_self = torch.zeros((input_size,1), requires_grad=True)
 
         # Initialize parameters for the feed forward layer
-        self.Wf = torch.empty(output_size, input_size)
-        init.xavier_normal_(self.Wf)
-        self.Wf.requires_grad = True
+        self.Wf = initialize_matrix(output_size, input_size)
         self.bf = torch.zeros((output_size, 1), requires_grad = True)
 
         # Initialize gamma, beta for layer normalization after feedforward layer
-        self.gamma_feed = torch.empty((hidden_size, output_size))
-        init.xavier_normal_(self.gamma_feed)
-        self.gamma_feed.requires_grad = True
+        self.gamma_feed = initialize_matrix(hidden_size, output_size)
         self.beta_feed = torch.zeros((input_size + output_size,1), requires_grad=True)
 
         # Initialize for the linear layer
-        self.Wl = torch.empty(1, input_size + output_size)
-        init.xavier_normal_(self.Wl)
-        self.Wl.requires_grad = True
+        self.Wl = initialize_matrix(1, input_size + output_size)
         self.bl = torch.zeros((1,1), requires_grad = True)    
     
-    # Positional encoding for the whole sentence
-    def positional_encoding(self, inputs):
-        pos_encoding = torch.empty(0, inputs.shape[1])
-        iter = inputs.shape[0]
-        d = inputs.shape[1]
-        position = torch.arange(iter).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d, 2) * (-math.log(10000.0) / d))
-        pe = torch.zeros(iter, d)
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        return pe + inputs
-
     # Positional encoding for the whole sentence
     # def positional_encoding(self, inputs):
     #     pos_encoding = torch.empty(0, inputs.shape[1])
     #     iter = inputs.shape[0]
-    #     for k in range(iter):
-    #         d = inputs.shape[1]
-    #         pos = torch.zeros((1,d))
-    #         for i in range(d):
-    #             if i % 2 == 0:
-    #                 pos[0][i] = np.sin((k/(self.n**(i/d))))
-    #             if i % 2 == 1:
-    #                 pos[0][i] = np.cos((k/(self.n**((i-1)/d))))
-    #         v_pos = inputs[k, :] + pos
-    #         pos_encoding = torch.cat((pos_encoding, v_pos), dim = 0)
-    #     return pos_encoding
+    #     d = inputs.shape[1]
+    #     position = torch.arange(iter).unsqueeze(1)
+    #     div_term = torch.exp(torch.arange(0, d, 2) * (-math.log(10000.0) / d))
+    #     pe = torch.zeros(iter, d)
+    #     pe[:, 0::2] = torch.sin(position * div_term)
+    #     pe[:, 1::2] = torch.cos(position * div_term)
+    #     return pe + inputs
+
+    # Positional encoding for the whole sentence
+    def positional_encoding(self, inputs):
+        pos_encoding = torch.empty(0, inputs.shape[1])
+        iter = inputs.shape[0]
+        for k in range(iter):
+            d = inputs.shape[1]
+            pos = torch.zeros((1,d))
+            for i in range(d):
+                if i % 2 == 0:
+                    pos[0][i] = np.sin((k/(self.n**(i/d))))
+                if i % 2 == 1:
+                    pos[0][i] = np.cos((k/(self.n**((i-1)/d))))
+            v_pos = inputs[k, :] + pos
+            pos_encoding = torch.cat((pos_encoding, v_pos), dim = 0)
+        return pos_encoding
     
     # Multi-head attention
     def multi_head_attention(self, inputs):
-        q = inputs @ self.Wq
-        k = inputs @ self.Wk
-        v = inputs @ self.Wv
-        attention = self_attention(self.mask, q, k, v)
-        output = attention @ self.Wo
+        # h is the cat matrix after we compute self attention of each head
+        h = torch.empty(inputs.shape[0], 0)
+        for i in range(len(self.Wq)):
+            qi = inputs @ self.Wq[i]
+            ki = inputs @ self.Wk[i]
+            vi = inputs @ self.Wv[i]
+            hi = self_attention(self.mask, qi, ki, vi)
+            # We concatenate hi to h by stacking rows 
+            h = torch.cat((h, hi), dim = 1)
+        output = h @ self.Wo
         return output
         
     # Layer norm after self-attention layer
@@ -154,7 +129,7 @@ class Transformer():
         y_pred = torch.softmax(output_linear, dim = 1)
         return y_pred
     
-    def process(self, X, y, run_backward = False):
+    def process(self, X, y, run_backward = False, lr = None):
         accuracy = 0
         for x, y_true in zip(X,y):
             # x is a matrix of a sentence
@@ -170,19 +145,26 @@ class Transformer():
                 self.optimizer.zero_grad()
                 L.backward()
                 self.optimizer.step()
+                # Update Wq, Wk, Wv manually
+                manually_update(self.Wq, lr)
+                manually_update(self.Wk, lr)
+                manually_update(self.Wv, lr)
          # Accuracy 
         return float(accuracy/len(X))
     
     def fit(self, X, y, max_iter = 201, learning_rate = 0.001, print_period = 20):
         self.loss = nn.BCELoss()
-        self.optimizer = opt.SGD([self.Wq, self.Wk, self.Wv, self.Wo, 
+        # Because Wq, Wk, Wv are tensor list
+        # and we are not using nn.Module
+        # We need to update those manually
+        self.optimizer = opt.SGD([self.Wo, 
                                   self.gamma_self, self.beta_self,
                                   self.Wf, self.bf, 
                                   self.gamma_feed, self.beta_feed,
                                   self.Wl, self.bl],
                                   lr = learning_rate)
         for i in range(max_iter):
-            accuracy = self.process(X, y, run_backward=True)
+            accuracy = self.process(X, y, run_backward=True, lr=learning_rate)
             if(i % print_period == 0):
                 print(f"Step: {i}")
                 print(f"accuracy for training data: {accuracy}")
--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
 import torch
 import numpy as np
+import torch.nn.init as init
+
+def initialize_matrix(n_rows, n_cols):
+    matrix = torch.empty(n_rows, n_cols)
+    init.xavier_normal_(matrix)
+    matrix.requires_grad = True
+    return matrix
+
+def stack_heads(n_heads, n_rows, n_cols):
+    tensors = [initialize_matrix(n_rows, n_cols) for _ in range(n_heads)]
+    return tensors
 
 def self_attention(mask, Q, K, V):
     att = (Q @ K.T)/np.sqrt(Q.shape[0]) + mask
     return (torch.softmax(att, dim = 1) @ V)
+
+def manually_update(tensor_list, lr):
+    with torch.no_grad():
+        for tensor in tensor_list:
+            tensor -= lr * tensor
+    for tensor in tensor_list:
+        tensor.grad.zero_()
